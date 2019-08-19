c2c9dfbdf6b6697fefdef784c16902ba230226e1
Lacour Vincent <lacour.vincent.app@gmail.com>
2019-08-19 14:40:23 +0200

    AppController in Kotlin

---
+++ app/src/main/java/com/lacour/vincent/hypnosedetente/screen/AppControler.kt
@@ -0,0 +1,31 @@
+package com.lacour.vincent.hypnosedetente.screen
+
+import android.app.Application
+import android.preference.PreferenceManager
+import androidx.appcompat.app.AppCompatDelegate
+
+class AppController : Application() {
+
+    companion object {
+        private const val THEME_KEY: String = "theme"
+        private const val THEME_DEFAULT_VALUE: String = "-1"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        initTheme()
+    }
+
+    private fun initTheme() {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        val prefsTheme = prefs.getString(THEME_KEY, THEME_DEFAULT_VALUE)
+        val theme = Integer.parseInt(prefsTheme!!)
+        when (theme) {
+            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            AppCompatDelegate.MODE_NIGHT_AUTO -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO)
+            AppCompatDelegate.MODE_NIGHT_NO -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            AppCompatDelegate.MODE_NIGHT_YES -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            else -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        }
+    }
+}
