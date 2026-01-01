83e1fa18472e23524c03908433dc69302b10606f
vlacour <lacour.vincent.app@gmail.com>
2026-01-01 16:15:51 +0100

    [tech] Use correct import of redux-saga middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,9 @@
 import { type Store, Tuple, configureStore as configureStoreFn } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
 
 import context from "@/store/context";
 import createRootReducer from "@/store/reducers";
 import sagas from "@/store/sagas";
-
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const createSagaMiddleware = require("redux-saga").default;
 
 export const createStore = (): { store: Store } => {
   const sagaMiddleware = createSagaMiddleware({ context });
