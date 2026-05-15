ec335805845d31a3bf20d1dae6c025b015a32694
vlacour <lacour.vincent.app@gmail.com>
2026-05-15 17:42:09 +0200

    [release] 2.4.0-SNAPSHOT

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ const config: ExpoConfig = {
   name: "Hypnose",
   slug: "hypnose-detente",
   owner: "lacour-vincent",
-  version: "2.3.0",
+  version: "2.4.0-SNAPSHOT",
   scheme: "hypnose-detente",
   platforms: ["android"],
   android: {
